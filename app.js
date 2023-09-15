2b06a8742eee914bacf465419d8c0a9016697db6
mattmarz94 <marzanomatt1994@gmail.com>
2023-09-14 20:20:09 -0400

    created the mobile nav menu for the 3 html files, and fixed their locations to coincide with the title of each destination. Also, adjusted the opacity of the drop down menu forg the mobile nav menu.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileMenuBtn = document.getElementById('mobile-menu-btn');
 const closeMenuBtn = document.getElementById('close-menu-btn');
+const viewMoreBlog = [...document.querySelectorAll("article.view-more-blog")]
 
+viewMoreBlog.forEach (blog => {
+    blog.style.display = "none";
+});
 
 document.addEventListener("click", function(e){
     if(e.target.classList.contains("fa-bars")){
@@ -20,8 +24,24 @@ document.addEventListener("click", function(e){
     } else if (e.target.classList.contains("nav-logo")){
         if (window.location.protocol === "http:")
             window.location.href = "../index.html"
+
+    } else if (e.target.classList.contains("view-more-link")){
+        const viewMore = document.querySelector("a.view-more-link")
+
+        e.preventDefault();
+
+        if (viewMoreBlog[0].style.display === "none"){
+            viewMoreBlog.forEach (blog => {
+                blog.style.display = "grid";
+            })
+                viewMore.textContent = "View less";
+        } else {
+            viewMoreBlog.forEach (blog => {
+                blog.style.display = "none";
+            })
+                viewMore.textContent = "View More"
         }
+    }
     
-    
-    
+
 })
